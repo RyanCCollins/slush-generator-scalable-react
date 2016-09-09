9718ef5cc26e17cecdc74a3c5af5fe4659fc7d7d
Ryan Collins <rccproductions@mac.com>
2016-09-09 18:47:07 -0400

    Fix: update serverport variable

--- templates/server/app.js
+++ templates/server/app.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 1337 : process.env.PORT;
+const serverPort = isDeveloping ? 1337 : process.env.PORT;
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -10,10 +10,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(port, '0.0.0.0', (err) => {
+app.listen(serverPort, '0.0.0.0', (err) => {
   if (err) {
     return console.warn(err);
   }
-  return console.info('==> 😎 Listening on port ${port}. Open http://0.0.0.0:' + port + '/ in your browser.');
+  return console.info('==> 😎 Listening on port ${port}. Open http://0.0.0.0:' + serverPort + '/ in your browser.');
 });
 /* eslint-enable */
